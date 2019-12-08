ed6ccd39e9035563263f962f85c96fd6f90ff5aa
binnn1234 <sjd3567@naver.com>
2019-12-08 13:19:19 +0900

    2019/12/08 공공기관데이터 통신모듈 기본 구조만 잡음

---
+++ Utill/GlobalUtil.swift
@@ -0,0 +1,9 @@
+//
+//  GlobalUtil.swift
+//  MiseMise_Toy
+//
+//  Created by base soft on 08/12/2019.
+//  Copyright © 2019 studygroup. All rights reserved.
+//
+
+import Foundation
---
+++ Utill/HttpCommThread.swift
@@ -0,0 +1,9 @@
+//
+//  HttpCommThread.swift
+//  MiseMise_Toy
+//
+//  Created by base soft on 08/12/2019.
+//  Copyright © 2019 studygroup. All rights reserved.
+//
+
+import Foundation
